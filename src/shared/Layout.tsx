1247b33317f99469323e71bcf6df3f55d2b760a7
Ginger Society Admin <admin@gingersociety.org>
2024-12-08 15:11:43 +0530

    feat: build fixes

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -4,46 +4,46 @@ import { Breadcrumb, BreadcrumbItem, SideMenu } from "@ginger-society/ginger-ui"
 import { ReactNode, useEffect, useState } from "react";
 import router from "./router";
 
-const Layout = ({children, breadcrumbConfig}: {children : ReactNode , breadcrumbConfig : BreadcrumbItem[]}) => {
+const Layout = ({ children, breadcrumbConfig }: { children: ReactNode, breadcrumbConfig: BreadcrumbItem[] }) => {
   const [activeItem, setActiveItem] = useState('home')
 
   useEffect(() => {
-    const matchedKey = Object.keys(pathIdMap).find(key => 
+    const matchedKey = Object.keys(pathIdMap).find(key =>
       router.state.location.pathname.startsWith(key)
     );
-  
+
     if (matchedKey) {
       setActiveItem(pathIdMap[matchedKey]);
     }
-  }, [router.state.location.pathname])
+  }, [])
 
   const handleMenuChange = (newId: string) => {
     router.navigate(idPathMap[newId])
-		setActiveItem(newId)
-	}
+    setActiveItem(newId)
+  }
 
   const handleBreadcrumbClick = (path: string) => {
     router.navigate(path);
-	}
+  }
 
   return (
     <div style={{ minHeight: "100vh" }}>
       <HeaderContainer />
       <div style={{ display: 'flex', height: 'calc(100vh)', paddingTop: '50px' }}>
-				<SideMenu
-					options={sideMenuOptions}
-					active={activeItem}
-					onChange={handleMenuChange}
-				/>
-        <div style={{padding: '20px', flexGrow: 1}}>
+        <SideMenu
+          options={sideMenuOptions}
+          active={activeItem}
+          onChange={handleMenuChange}
+        />
+        <div style={{ padding: '20px', flexGrow: 1 }}>
           <Breadcrumb value={breadcrumbConfig} onClick={handleBreadcrumbClick} />
 
           {children}
-    
+
 
         </div>
 
-        </div>
+      </div>
     </div>
   );
 };
