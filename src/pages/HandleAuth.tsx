b9c7f2ed5f0c14dbace945cf1b8d54154d466c27
Ginger Society Admin <admin@gingersociety.org>
2024-12-14 14:27:47 +0530

    feat: added return url

--- src/pages/HandleAuth.tsx
+++ src/pages/HandleAuth.tsx
@@ -2,8 +2,14 @@ import router from "@/shared/router";
 import { HandleLoginRedirect } from "@ginger-society/ginger-ui";
 
 export const HandleAuth = () => {
+
   const navigateToHome = () => {
-    router.navigate("/users");
+    const path = router.state.location.search.split('?returnUrl=')[1]
+    if (path) {
+      router.navigate(path);
+    } else {
+      router.navigate("/users");
+    }
   };
   return <HandleLoginRedirect handleNavigation={navigateToHome} />;
 };
